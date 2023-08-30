61518c8cd50aacbede4a0dc7c4ed3b9d5cbe8ae1
edgarfuste <34737125+edgarfuste@users.noreply.github.com>
2023-08-30 11:54:31 +0200

    Update headers files

--- GwSetRolePlugin.py
+++ GwSetRolePlugin.py
@@ -1,3 +1,10 @@
+"""
+Copyright © 2023 by BGEO. All rights reserved.
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU
+General Public License as published by the Free Software Foundation, either version 3 of the License,
+or (at your option) any later version.
+"""
+
 from qgis.server import *
 from qgis.core import *
 
--- __init__.py
+++ __init__.py
@@ -1,3 +1,9 @@
+"""
+Copyright © 2023 by BGEO. All rights reserved.
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU
+General Public License as published by the Free Software Foundation, either version 3 of the License,
+or (at your option) any later version.
+"""
 
 def serverClassFactory(serverIface):
     from .GwSetRolePlugin import GwSetRolePlugin
